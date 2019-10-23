cbc304fe67eceee36b16a89cc4039179877b9b6d
Victor Pragana <victormerigue@gmail.com>
2019-10-23 11:10:06 -0300

    novo teste

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -1,11 +1,11 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the "Offline page" service worker
 
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
 // Check compatibility for the browser we're running this in
 if ("serviceWorker" in navigator) {
   if (navigator.serviceWorker.controller) {
-    console.log("active service worker found, no need to register");
+    console.log("[PWA Builder] active service worker found, no need to register");
   } else {
     // Register the service worker
     navigator.serviceWorker
@@ -13,7 +13,7 @@ if ("serviceWorker" in navigator) {
         scope: "./"
       })
       .then(function(reg) {
-        console.log("Service worker has been registered for scope: " + reg.scope);
+        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
       });
   }
 }
